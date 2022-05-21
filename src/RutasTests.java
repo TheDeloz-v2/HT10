c454e560432eaadf911eb4792d76b7a11ab39484
Fernanda Esquivel <esq21542@uvg.edu.gt>
2022-05-20 19:58:04 -0600

    Rutas JUnit implemented

--- src/RutasTests.java
+++ src/RutasTests.java
@@ -5,9 +5,17 @@ import org.junit.jupiter.api.Test;
 class RutasTests {
 
 	@Test
-	void modifyTest() {
+	void createTest() {
 		Rutas rts = new Rutas("pruebaGrafo.txt");
 		rts.createRoute("Hola", "Adios", 69);
 		assertEquals(true,  rts.routeExists("Hola", "Adios"));
+	}
+	
+	@Test
+	void deleteTest() {
+		Rutas rts = new Rutas("pruebaGrafo.txt");
+		rts.createRoute("Dia", "Noche", 13);
+		rts.deleteRoute("Dia", "Noche");
+		assertEquals(false,  rts.routeExists("Dia", "Noche"));
 	}
 }
